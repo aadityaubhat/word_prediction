4885dc8e4741e748f05ed2ae9660ea982f1c3683
Aaditya Bhat <aadityaubhat@gmail.com>
2017-06-24 19:00:12 -0400

    Completed working prototype of next word prediction

--- app_word_prediction/app.R
+++ app_word_prediction/app.R
@@ -1,13 +1,7 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Setting up environment
 library(shiny)
+setwd("~/Documents/rWorkspace/word_prediction/app_word_prediction/")
+source("wordPrediction.r")
 
 # Define UI for application that draws a histogram
 if (interactive()) {
@@ -28,16 +22,8 @@ if (interactive()) {
     )
   )
   
-  # Dummy Function to generate three words
-  generateWords <- function(wordList){
-    if(length(wordList) >= 3){
-      return(c("one", "two","three"))
-    } else if(length(wordList) == 2) {
-      return(c("one", "two","one"))
-    } else {
-      return(c("one", "one","one"))
-    }
-  }
+  # Trim function
+  trim <- function (x) gsub("^\\s+|\\s+$", "", x)
   
   server <- function(input, output, session) {
     
@@ -48,7 +34,7 @@ if (interactive()) {
       word1 <- threeWords$currentValue[1]
       
       # This will change the value of input$inText, based on word1 Action Button
-      updateTextAreaInput(session, "inText", value = paste(input$inText, word1))
+      updateTextAreaInput(session, "inText", value = paste0(input$inText, word1," "))
     })
     
     # Observe Button 2 Click
@@ -56,7 +42,7 @@ if (interactive()) {
       word2 <- threeWords$currentValue[2]
       
       # This will change the value of input$inText, based on word3 Action Button
-      updateTextAreaInput(session, "inText", value = paste(input$inText, word2))
+      updateTextAreaInput(session, "inText", value = paste0(input$inText, word2, " "))
     })
     
     # Observe Button 3 Click
@@ -64,7 +50,7 @@ if (interactive()) {
       word3 <- threeWords$currentValue[3]
       
       # This will change the value of input$inText, based on word3 Action Button
-      updateTextAreaInput(session, "inText", value = paste(input$inText, word3))
+      updateTextAreaInput(session, "inText", value = paste0(input$inText, word3, " "))
     })
     
     #Observe Text Area
@@ -73,13 +59,22 @@ if (interactive()) {
       
       
       if(inputText != ""){
+        
         #Get Vector of Words 
         words <- unlist(strsplit(inputText, " "))
         
-        threeWords$currentValue <- generateWords(words)
+        lastChar <- nchar(inputText)
         
-        updateActionButton(session = session, inputId = "word1Click", label = threeWords$currentValue[1])
+        trim(inputText)
+        
+        if(substr(inputText, lastChar, lastChar) == " "){
+          print(inputText)
+          threeWords$currentValue <- predictNext(inputText)
+          print(threeWords$currentValue)
+        }
+        
         updateActionButton(session = session, inputId = "word2Click", label = threeWords$currentValue[2])
+        updateActionButton(session = session, inputId = "word1Click", label = threeWords$currentValue[1])
         updateActionButton(session = session, inputId = "word3Click", label = threeWords$currentValue[3])
       }
       
