e332a12617134902c8d66882e92c338fcaf4f9ed
Aaditya Bhat <aadityaubhat@gmail.com>
2017-06-24 17:46:14 -0400

    Improved csv reading performance, completed naive prediction function

--- app_word_prediction/wordPrediction.r
+++ app_word_prediction/wordPrediction.r
@@ -1,6 +1,8 @@
+# Setting up the environment ####
 # Load Required Libraries 
 library(data.table)
 library(dplyr)
+library(data.table)
 
 
 # Load word frequencies ####
@@ -9,19 +11,12 @@ wordFreqFiles <- list.files("Documents/rWorkspace/word_prediction/sparkWordCount
 
 wordFreqFiles <- wordFreqFiles[grep("part*", wordFreqFiles)]
 
-wordFreq <- data.frame(word = character(),
-                       count = integer())
+wordFreqFiles <- paste0("Documents/rWorkspace/word_prediction/sparkWordCount/outputFiles/output/counts/", wordFreqFiles)
 
-for(fileName in wordFreqFiles){
-  this.file <- read.csv(paste0("Documents/rWorkspace/word_prediction/sparkWordCount/outputFiles/output/counts/",
-                               fileName), header = F)
-  
-  names(this.file) <- c("word", "count")
-  
-  wordFreq <- rbind(wordFreq, this.file)
-  
-  rm(this.file)
-}
+lst <- lapply(wordFreqFiles, fread)
+wordFreq <- rbindlist(lst)
+names(wordFreq) <- c("word", "count")
+
 
 
 # Load Bigrams ####
@@ -30,19 +25,13 @@ bigramFiles <- list.files("Documents/rWorkspace/word_prediction/sparkWordCount/o
 
 bigramFiles <- bigramFiles[grep("part*", bigramFiles)]
 
-bigrams <- data.frame(bigram = character(),
-                       count = integer())
+bigramFiles <- paste0("Documents/rWorkspace/word_prediction/sparkWordCount/outputFiles/output/bigrams/",
+                      bigramFiles)
 
-for(fileName in bigramFiles){
-  this.file <- read.csv(paste0("Documents/rWorkspace/word_prediction/sparkWordCount/outputFiles/output/bigrams/",
-                               fileName), header = F)
-  
-  names(this.file) <- c("bigram", "count")
-  
-  bigrams <- rbind(bigrams, this.file)
-  
-  rm(this.file)
-}
+lst <- lapply(bigramFiles, fread)
+bigrams <- rbindlist(lst)
+names(bigrams) <- c("bigram", "count")
+
 
 
 
@@ -52,29 +41,26 @@ trigramFiles <- list.files("Documents/rWorkspace/word_prediction/sparkWordCount/
 
 trigramFiles <- trigramFiles[grep("part*", trigramFiles)]
 
-trigrams <- data.frame(trigrams = character(),
-                      count = integer())
+trigramFiles <- paste0("Documents/rWorkspace/word_prediction/sparkWordCount/outputFiles/output/trigrams/", trigramFiles)
 
-for(fileName in trigramFiles){
-  this.file <- read.csv(paste0("Documents/rWorkspace/word_prediction/sparkWordCount/outputFiles/output/trigrams/",
-                               fileName), header = F)
-  
-  names(this.file) <- c("trigram", "count")
-  
-  trigrams <- rbind(trigrams, this.file)
-  
-  rm(this.file)
-}
+lst <- lapply(trigramFiles, fread)
+trigrams <- rbindlist(lst)
+names(trigrams) <- c("trigram", "count")
 
 
-# Filter bigrams and trigrams with single occurence 
+
+
+# Data Cleanning and Processing ####
+# Filter wordFreq, bigrams and trigrams 
+
+
 # bigrams
 bigrams <- data.table(bigrams)
 
 nrow(bigrams)
 # 6360154
 
-bigrams <- bigrams %>% filter(count > 4)
+bigrams <- bigrams %>% filter(count > 15)
 
 nrow(bigrams)
 # 630075
@@ -87,7 +73,7 @@ trigrams <- data.table(trigrams)
 nrow(trigrams)
 # 19381011
 
-trigrams <- trigrams %>% filter(count > 4)
+trigrams <- trigrams %>% filter(count > 15)
 
 nrow(trigrams)
 # 709065
@@ -95,20 +81,37 @@ nrow(trigrams)
 hist(log(trigrams$count))
 
 
-# Naive prediction
+# Naive prediction ####
 
-trigrams <- trigrams %>% arrange(desc(count))
-bigrams <- bigrams %>% arrange(desc(count))
+trigrams <- data.table(trigrams %>% arrange(desc(count)))
+bigrams <- data.table(bigrams %>% arrange(desc(count)))
 
-predict() <- function(phrase){
-  phrase = "The Day is"
+predictNext <- function(phrase){
   phrase <- tolower(phrase)
   words <- unlist(strsplit(phrase, " "))
   
   # When phrase has two or more words
   lastTwoWords <- words[(length(words)-1):length(words)]
   
-  trigramResult <- trigrams[grepl(paste0("^",paste(lastTwoWords, collapse = " " ), ".*"), trigrams$trigram),]
+  # Assuming there are three or more matching trigrams
+  trigramResult <- trigrams[grepl(paste0("^",paste(lastTwoWords, collapse = " " ), ".*"), trigrams$trigram),][1:3,]$trigram
   
-  trigramPredictions <- strsplit(trigramResult[1:3,]$trigram, " ")
+  trigramResult <- unlist(strsplit(trigramResult, " ", fixed = ))[c(3,6,9)]
+  
+  bigramResult <- bigrams[grepl(paste0("^",lastTwoWords[length(lastTwoWords)], ".*"), bigrams$bigram),][1:3,]$bigram
+  
+  bigramResult <- unlist(strsplit(bigramResult, " ", fixed = ))[c(2,4,6)]
+  
+  result <- c(trigramResult, bigramResult)
+  
+  result <- unique(result[!is.na(result)])
+
+  return(result[1:3])
 }
+
+# Testing ####
+predictNext("")
+
+predictNext("How are")
+
+predictNext("Why not")
